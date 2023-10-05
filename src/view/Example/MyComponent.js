bc6b7f4ea6d677cf747e678d728fd7fc97d0d61f
kieudgwa <le-thi-my-kieu>
2023-10-05 16:01:19 +0700

    state kiem soat trang thai ung dung react

--- src/view/Example/MyComponent.js
+++ src/view/Example/MyComponent.js
@@ -1,9 +1,31 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  /**
+   *  JSX
+   *  Fragment
+   */
+  state = {
+    name: 'kieu',
+    chanel: 'hoi dan IT'
+  }
+  handleOnChangeName = (event) =>  {
+    this.setState({
+      name: event.target.value
+    })
+  }
   render() {
     return(
-      <div>Hello my component</div>
+      <React.Fragment>
+      <div className="first">
+          <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)}></input>
+          Hello {this.state.name}
+      </div>
+      <div className="second">
+        My youtube chanel: { this.state.chanel }
+      </div>
+      </React.Fragment>
+  
     )
   }
 }
