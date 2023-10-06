0b7854478f44ff2779407c8d81caee66cebbece9
kieudgwa <le-thi-my-kieu>
2023-10-06 09:24:15 +0700

    form in react js

--- src/view/Example/MyComponent.js
+++ src/view/Example/MyComponent.js
@@ -6,34 +6,36 @@ class MyComponent extends React.Component {
    *  Fragment
    */
   state = {
-    name: '',
-    chanel: 'hoi dan IT'
+    firstName: '',
+    lastName: ''
   }
-  handleOnChangeName = (event) =>  {
+  handleChangeFirstName = (event) => {
     this.setState({
-      name: event.target.value,
-      chanel: 'abc'
+      firstName: event.target.value
     })
   }
-  handleClickButton = () => {
-    alert('click me')
+  handleChangeLastName = (event) => {
+    this.setState({
+      lastName: event.target.value
+    })
   }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('data', this.state)
+  }
+
   render() {
-    console.log('>>> call render: ', this.state);
-    return(
+    console.log('>> render', this.state);
+    return (
       <>
-      <div className="first">
-          <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)}></input>
-          Hello {this.state.name}
-      </div>
-      <div className="second">
-        My youtube chanel: { this.state.chanel }
-      </div>
-      <div className="third">
-          <button onClick={ () => this.handleClickButton() }>Click me</button>
-      </div>
+        <form>
+          <label htmlFor="fname">First name:</label><br />
+          <input type="text" id="fname" name="fname" value={this.state.firstName} onChange={(event) => this.handleChangeFirstName(event)} /><br />
+          <label htmlFor="lname">Last name:</label><br />
+          <input type="text" id="lname" name="lname" value={this.state.lastName} onChange={(event) => this.handleChangeLastName(event)} /><br /><br />
+          <input type="submit" value="Submit" onClick={(e) => this.handleSubmit(e)} />
+        </form>
       </>
-  
     )
   }
 }
