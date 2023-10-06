aaee42924319da90af379b99ebd85cb9fcce7662
kieudgwa <le-thi-my-kieu>
2023-10-06 10:33:55 +0700

    props

---
+++ src/view/Example/ChildComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class ChildComponent extends React.Component {
+  render() {
+    // let name = this.props.name;
+    // let age = this.props.age;
+    let { name, age, arrJobs } = this.props; // destructuring assignment
+    console.log('>> check props', this.props);
+    let a = '';
+    return (
+      <>
+        <div>Child Component name: {name}, age: {age}</div>
+        <div className='job-lits'>
+          {
+            a = arrJobs.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {item.title} - {item.salary}
+                </div>
+              )
+            })
+          }
+          {console.log('>> check map araay', a)}
+        </div>
+      </>
+    );
+  }
+}
+
+export default ChildComponent;
