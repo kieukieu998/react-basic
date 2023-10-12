5afe9ea7f5fca6398731d760753f6e7f1393cbf3
kieudgwa <le-thi-my-kieu>
2023-10-12 16:40:22 +0700

    importing image

--- src/view/Example/Home.js
+++ src/view/Example/Home.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
+import logo from '../../assets/images/logo.png'
 
 class Home extends React.Component {
   componentDidMount() {
     setTimeout(() => {
       console.log(123);
       // this.props.history.push('/todo')
-    },3000)
+    }, 3000)
   }
   // HOC: highter order component
   render() {
     console.log('>> check props', this.props);
-    return(
-      <div>Hello Home Component</div>
+    return (
+      <>
+        <div>Hello Home Component</div>
+        <div>
+          <img src={logo} style={{width: 100}}/>
+        </div>
+      </>
     )
   }
 }
