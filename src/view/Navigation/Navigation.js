bafc5d37d8b591d1837c8ff19090ac201c55b185
kieudgwa <le-thi-my-kieu>
2023-10-12 11:30:54 +0700

    Links và NavLinks

--- src/view/Navigation/Navigation.js
+++ src/view/Navigation/Navigation.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './Navigation.scss'
+import {
+  Link, NavLink
+} from "react-router-dom";
 class Navigation extends React.Component {
   render() {
     return (
       <>
         <div className="topnav">
-          <a className="active" href="/">Home</a>
-          <a href="/todo">Todo</a>
-          <a href="/about">About</a>
+          <NavLink activeClassName="active" to="/" exact = {true}>Home</NavLink>
+          <NavLink activeClassName="active" to="/todo">Todo</NavLink>
+          <NavLink activeClassName="active" to="/about">About</NavLink>
         </div>
       </>
     )
